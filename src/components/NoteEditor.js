fbccf6402c3e4a4f1972d554d431704198208f26
Sam Noh <samnoh21@outlook.com>
2019-07-22 10:16:12 +1200

    add - editorContainer

--- src/components/NoteEditor.js
+++ src/components/NoteEditor.js
@@ -1,43 +1,13 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import { withRouter } from 'react-router-dom';
 
 import { Title } from '../styles/titles';
-import { GrayButton, AddButton, ShowButton } from '../styles/buttons';
 import { EditorContainer, TitleInput, ContentContainer, ContentInput } from '../styles/editor';
 import MarkdownStyle from '../styles/markdown';
 
-const NoteEditor = ({ id, title = '', content = '', submit, history }) => {
-    const [titleVal, setTitleVal] = useState(title);
-    const [contentVal, setContentVal] = useState(content);
-    const [editView, toggleEditview] = useState(true);
-
-    const submitNote = useCallback(() => {
-        if (titleVal === '' || contentVal === '') {
-            alert('No');
-            return;
-        }
-        submit({ variables: { id, title: titleVal, content: contentVal } });
-        history.push('/');
-    }, [id, titleVal, contentVal, submit, history]);
-
-    const resetVals = useCallback(() => {
-        if (window.confirm('Are you sure to reset?')) {
-            setTitleVal(title ? title : '');
-            setContentVal(content ? content : '');
-        }
-    }, [title, content, setTitleVal, setContentVal]);
-
+const NoteEditor = ({ id, titleVal, contentVal, editView, setTitleVal, setContentVal }) => {
     return (
         <>
-            <GrayButton onClick={() => history.push('/')} left>
-                Back
-            </GrayButton>
-            <GrayButton onClick={resetVals}>Reset</GrayButton>
-            <AddButton onClick={submitNote}>Save</AddButton>
-            <ShowButton onClick={() => toggleEditview(!editView)}>
-                {editView ? 'Preview' : 'Edit'}
-            </ShowButton>
             <Title>{id ? 'Edit Note' : 'Add Note'}</Title>
             <EditorContainer>
                 <TitleInput
@@ -63,4 +33,4 @@ const NoteEditor = ({ id, title = '', content = '', submit, history }) => {
     );
 };
 
-export default withRouter(NoteEditor);
+export default NoteEditor;
--- src/containers/EditorContainer.js
+++ src/containers/EditorContainer.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { withRouter } from 'react-router-dom';
 
 import NoteEditor from '../components/NoteEditor';
+import { GrayButton, AddButton, ShowButton } from '../styles/buttons';
 
-const EditorContainer = props => {
+const EditorContainer = ({ id, title = '', content = '', submit, history }) => {
+    const [titleVal, setTitleVal] = useState(title);
+    const [contentVal, setContentVal] = useState(content);
+    const [editView, toggleEditview] = useState(true);
+
+    const resetVals = useCallback(() => {
+        if (window.confirm('Are you sure to reset?')) {
+            setTitleVal(title ? title : '');
+            setContentVal(content ? content : '');
+        }
+    }, [title, content, setTitleVal, setContentVal]);
+
+    const submitNote = useCallback(() => {
+        if (titleVal === '' || contentVal === '') {
+            alert('Your note is empty...');
+            return;
+        }
+
+        submit({ variables: { id, title: titleVal, content: contentVal } });
+        history.push(`/note/${id}`);
+    }, [id, titleVal, contentVal, submit, history]);
+
     return (
         <>
-            <NoteEditor {...props} />
+            <GrayButton onClick={() => history.push('/')} left>
+                Back
+            </GrayButton>
+            <GrayButton onClick={resetVals}>Reset</GrayButton>
+            <AddButton onClick={submitNote}>Save</AddButton>
+            <ShowButton onClick={() => toggleEditview(!editView)}>
+                {editView ? 'Preview' : 'Edit'}
+            </ShowButton>
+            <NoteEditor {...{ id, titleVal, contentVal, editView, setTitleVal, setContentVal }} />
         </>
     );
 };
 
-export default EditorContainer;
+export default withRouter(EditorContainer);
